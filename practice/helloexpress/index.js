bf327fae65138147f7b30ec8a2f0553c2f2b4aa5
hollowdll <pixerchat@gmail.com>
2023-01-27 13:42:20 +0200

    first express app

--- practice/helloexpress/index.js
+++ practice/helloexpress/index.js
@@ -4,9 +4,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (_req, res) => {
-    console.log("New request to endpoint '/'");
+    console.log("Request to  '/'");
     res.send("Hello Express!");
-})
+});
+
+app.get("/home/:user", (req, res) => {
+    console.log("Request to  '/home/user'");
+    res.send(req.params.user);
+});
+
+app.get("/about", (_req, res) => {
+    console.log("Request to  '/about'");
+    res.send("<h2>About page</h2>")
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
